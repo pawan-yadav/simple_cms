b2acc3e44c191f2188a1ffbcfeab16161c3a05d4
Hunt Burdick <hunt.burdick@musclesound.com>
2015-07-18 22:15:53 -0600

    allow user to change password.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]                                          
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
 
   root :to => 'pages#show', :id => '1'
   get 'admin' => "admin/admin#index"
