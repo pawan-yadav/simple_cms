23b967734c4805f20ff8e73d7796f1418503d74d
Hunt Burdick <hunt.burdick@musclesound.com>
2015-07-28 10:01:21 -0600

    default sitemap.

--- sitemap.rb
+++ sitemap.rb
@@ -1,9 +1,10 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'http://www.saucedobrothers.com'
+SitemapGenerator::Sitemap.default_host = 'http://www.website.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
+  add '/blog', :changefreq => 'weekly'
+  add '/contact', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
