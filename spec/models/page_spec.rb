0c880657f6f19fa6c899b8aa64d96cfda51534c2
Hunt Burdick <hunt.burdick@musclesound.com>
2015-07-31 08:44:22 -0600

    building out tests.

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Page, type: :model do
 
+  it "can be created" do
+    page = FactoryGirl.build(:page, name: 'Example Page')
+
+    expect(page).to be_valid
+  end
+
+
+  it "has a slug after creation" do
+    example_page = Page.create!(name: 'This Page')
+
+    expect(example_page.slug).to eq('this-page')
+  end
+  
+
   it "orders by name" do
     services = Page.create!(name: "Services")
     about = Page.create!(name: "About")
 
     expect(Page.order('name ASC')).to eq([about, services])
-  end
-
-
-  it "has a slug after creation" do
-  	example_page = Page.create!(name: 'Example Page')
-
-  	expect(example_page.slug).to eq('example-page')
-  end
-
-
-  it "can be created with validations" do
-  	page = FactoryGirl.build(:page, name: 'Example Page')
-
-    expect(page).to be_valid
   end
 
 
