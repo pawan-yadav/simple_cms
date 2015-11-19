7ff94926a76354b774a64a81847ad5d8b28ec5bc
Hunt Burdick <hunt@websitescenes.com>
2015-11-19 16:37:58 -0700

    lock down users in demo.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -18,42 +18,48 @@ module Admin
 	  end
 
 	  def create
-	    @user = User.new(:email => params[:user][:email], :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
+	    # @user = User.new(:email => params[:user][:email], :password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
 
-      if @user.save
-      	flash[:notice] = "User successfully created."
-        redirect_to :controller => 'admin/users'
-      else
-        render action: 'new'
-      end
+      #  if @user.save
+      #  	flash[:notice] = "User successfully created."
+      #    redirect_to :controller => 'admin/users'
+      #  else
+      #    render action: 'new'
+      #  end
+      flash[:notice] = "Users cannot be updated in the demo."
+      redirect_to :action => 'index'
 	  end
 
 	  def update
-	    if user_params[:password].blank?
-	      user_params.delete("password")
-	      user_params.delete("password_confirmation")
-	    end
+	    # if user_params[:password].blank?
+	    #   user_params.delete("password")
+	    #   user_params.delete("password_confirmation")
+	    # end
 
 
-	    if @user.update(user_params)
-	    	flash[:notice] = "User successfully updated."
-	      redirect_to :action => 'edit'
-	    else
-	      render action: 'edit'
-	    end
+	    # if @user.update(user_params)
+	    # 	flash[:notice] = "User successfully updated."
+	    #   redirect_to :action => 'edit'
+	    # else
+	    #   render action: 'edit'
+	    # end
+	    flash[:notice] = "Users cannot be updated in the demo."
+	    redirect_to :action => 'index'
 	  end
 
 	  
 	  def destroy
 	  	@user = User.find(params[:user_id])
 
-	    unless @user.id == 1
-	      @user.destroy
-	    end
+	    # unless @user.id == 1
+	    #   @user.destroy
+	    # end
 
-	    respond_to do |format|
-        format.js { list_refresh }
-      end
+	    # respond_to do |format|
+     #    format.js { list_refresh }
+     #  end
+      flash[:notice] = "Users cannot be updated in the demo."
+	    redirect_to :action => 'index'
 	  end
 
 
